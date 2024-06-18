1a9894b8221dbbc2b35a66f548abc155c7098414
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-06-18 18:01:04 -0300

    update

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,26 +1,24 @@
+using Microsoft.AspNetCore.Components.Web;
 using ScreenSound.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var app = builder.Build();
+builder.Services.AddRazorComponents();
 
-// Configure the HTTP request pipeline.
+
+var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
